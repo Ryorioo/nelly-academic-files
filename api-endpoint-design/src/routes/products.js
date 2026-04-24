b4106bcf26f03b5a94525daa3e1de044736b3c6a
Ryorioo <neellyy29@gmail.com>
2026-04-24 21:34:38 +0800

    Delete api-endpoint-design directory

--- api-endpoint-design/src/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProducts, getProductById, getProductReviews } = require('../controllers/productsController');
-
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-router.get('/:productId/reviews', getProductReviews);
-
-module.exports = router;
--- api-endpoint-design/src/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllUsers, getUserOrders } = require('../controllers/usersController');
-
-router.get('/', getAllUsers);
-router.get('/:userId/orders', getUserOrders);
-
-module.exports = router;
