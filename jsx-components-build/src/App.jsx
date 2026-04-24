9000bb48eb5b3404a6f7b330a41cf1100704653c
Ryorioo <neellyy29@gmail.com>
2026-04-24 22:55:40 +0800

    Add files via upload

---
+++ jsx-components-build/src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from "./Welcome";
+import UserCard from "./UserCard";
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <UserCard name="Nelly" email="neellyy29@gmail.com" />
+      <UserCard name="Jhon" email="mercadojd.626.stud@udd.edu.ph" />
+    </div>
+  );
+}
+
+export default App;
